fcbca3013848b2907fb495047648acf52b4e217b
Tabbu <50023740+Typachil@users.noreply.github.com>
2024-11-07 12:06:27 +0500

    fix: fix config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import IndexPage from "./index.tsx";
 import FormPage from "@/form.tsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <IndexPage />,
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build : {
+    outDir: "build"
+  },
   plugins: [react()],
   resolve: {
     alias: {
